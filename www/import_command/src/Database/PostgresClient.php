18f5e27a46af2beea7b90da1c9af4ea1f35ce10f
Dmitry-Poletaev <52959973+Dmitry-Poletaev@users.noreply.github.com>
2023-08-28 01:07:43 +0700

    Update PostgresClient.php

--- www/import_command/src/Database/PostgresClient.php
+++ www/import_command/src/Database/PostgresClient.php
@@ -13,37 +13,5 @@ class PostgresClient extends BaseClient
             dd($e->getMessage());
         }
     }
-
-//    public function importData(array $data): void
-//    {
-//        array_shift($data); // Remove the first line (header)
-//
-//        $this->pdo->beginTransaction();
-//
-//        try {
-//            $stmt = $this->pdo->prepare("INSERT INTO imported_data (uid, ctime, event_name) VALUES (:uid, :ctime, :event_name)");
-//            foreach ($data as $line) {
-//                //Clean the line from double quotes
-//                $pieces = str_getcsv($line, ',');
-//                //$cleanedLine = preg_replace('/"([^"]*)","([^"]*)","([^"]*)"/', '$1,$2,$3', $pieces[0]);
-//                $cleanedLine = str_replace('"', '', $pieces[0]);
-//                $columns = explode(',', $cleanedLine);
-//
-//                $col1 = $columns[0];
-//                $col2 = $columns[1];
-//                $col3 = $columns[2];
-//
-//                $stmt->bindParam(':uid', $col1);
-//                $stmt->bindParam(':ctime', $col2);
-//                $stmt->bindParam(':event_name', $col3);
-//
-//                $stmt->execute();
-//            }
-//            $this->pdo->commit();
-//        } catch (\PDOException $e) {
-//            $this->pdo->rollBack();
-//            dd($e->getMessage());
-//        }
-//
-//    }
+    
 }
